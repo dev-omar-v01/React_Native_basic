1c9a50ce63ea81556979c33cbda9b65895c8d55a
softdevomar <omorfarukqqq@gmail.com>
2023-10-29 00:34:53 -0700

    init

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import FlexDirectionBasics from './project_file/Card';
+
+export default function App() {
+  return <FlexDirectionBasics />;
+}
